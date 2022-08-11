6dc5722b2369ca5bc70122f634439ebe71dc6858
AlexKonopada <6761240@gmail.com>
2022-08-11 23:04:35 +0300

    good afternoon

--- src/Tiget.java
+++ src/Tiget.java
@@ -2,5 +2,6 @@ public class Tiget {
     public static void main(String[] args) {
         System.out.println("Tiger");
         System.out.println("Hi");
+        System.out.println("Good afternoon");
     }
 }
